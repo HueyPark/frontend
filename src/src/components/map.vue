f66fd2eb47149715c50a98b1497790a441d7aa5d
HueyPark <jaewan.huey.park@gmail.com>
2017-03-18 12:31:04 +0900

    Create create unit button

--- src/src/components/map.vue
+++ src/src/components/map.vue
@@ -1,7 +1,14 @@
 <template>
 <div>
-  <div style="width:95vw; height:80vh; border: 1px solid lightgray; margin: 0 auto" :id="visId"></div>
-  <div>
+  <div class="panel panel-default">
+    <div class="panel-body" style="height:80vh;" :id="visId"></div>
+  </div>
+  <div v-if="selectedNodeId !== undefined" class="panel panel-default">
+    <div class="panel-body">
+      <div class="btn-group" role="group">
+        <button @click="createUnit" type="button" class="btn btn-default">Create Unit</button>
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -12,7 +19,7 @@ import restClient from '../framework/rest_client.js'
 export default {
   data () {
     return {
-      nodes: [1, 2, 3]
+      selectedNodeId: undefined
     }
   },
   computed: {
@@ -44,33 +51,43 @@ export default {
       if (sqDist > (100 * 100)) return false
 
       return true
+    },
+    createUnit: function () {
+      restClient.post(
+        '/units',
+        function (res) {
+          console.log(res)
+        },
+        {node_id: this.selectedNodeId})
+    },
+    readNodes: function () {
+      var self = this
+      restClient.get('/nodes', function (res) {
+        /* eslint-disable no-new */
+        /* eslint-disable no-undef */
+        let nodes = new vis.DataSet(res.data.nodes.map(function (node) {
+          return {
+            id: node.id,
+            fixed: true,
+            x: node.pos.x,
+            y: node.pos.y
+          }
+        }))
+
+        let data = {
+          nodes: nodes,
+          edges: self.generateEdges(res.data.nodes)
+        }
+
+        let network = new vis.Network(document.getElementById(self.visId), data, {})
+        network.on('click', function (data) {
+          self.selectedNodeId = data.nodes[0]
+        })
+      })
     }
   },
   mounted: function () {
-    var self = this
-    restClient.get('/nodes', function (res) {
-      /* eslint-disable no-new */
-      /* eslint-disable no-undef */
-      let nodes = new vis.DataSet(res.data.nodes.map(function (node) {
-        return {
-          id: node.id,
-          fixed: true,
-          x: node.pos.x,
-          y: node.pos.y
-        }
-      }))
-
-      let data = {
-        nodes: nodes,
-        edges: self.generateEdges(res.data.nodes)
-      }
-
-      let network = new vis.Network(document.getElementById(self.visId), data, {})
-      network.on('selectNode', function (data) {
-        let node = nodes.get(data.nodes[0])
-        console.log(node)
-      })
-    })
+    this.readNodes()
   }
 }
 </script>
--- src/src/framework/rest_client.js
+++ src/src/framework/rest_client.js
@@ -3,6 +3,13 @@ import axios from 'axios'
 const url = 'http://127.0.0.1:5000'
 
 export default {
+  post: function (endpoint, callback, data) {
+    axios.post(url + endpoint, data, { headers: {
+      'Content-Type': 'application/json'
+    }}).then(function (res) {
+      callback(res)
+    })
+  },
   get: function (endpoint, callback) {
     axios.get(url + endpoint).then(function (res) {
       callback(res)
